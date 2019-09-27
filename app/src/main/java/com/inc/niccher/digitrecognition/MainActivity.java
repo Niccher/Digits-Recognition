1cff2e2e0217b27facf361f585a5ba37d40bfb2e
Nicch <domi777nicch@gmail.com>
2019-09-27 11:11:21 +0300

    Added Pre-trained inference assets(.pb),Tensorflow classifier class and other auxilliary classes.

--- app/src/main/java/com/inc/niccher/digitrecognition/MainActivity.java
+++ app/src/main/java/com/inc/niccher/digitrecognition/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-
 import com.inc.niccher.digitrecognition.views.DrawModel;
 import com.inc.niccher.digitrecognition.views.DrawView;
 
